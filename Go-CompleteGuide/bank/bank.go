8ac0169a526400a66c9a6c266289b18dd4c12b5f
NorieM <norie.macewan@tiscali.co.uk>
2025-04-01 00:33:51 +0100

    Added functionality to bank to read balance from file.

--- Go-CompleteGuide/bank/bank.go
+++ Go-CompleteGuide/bank/bank.go
@@ -2,15 +2,26 @@ package main
 
 import "fmt"
 import "os"
+import "strconv"
+
+const accountBalanceFile = "balance.txt"
 
 func writeBalanceToFile(balance float64) {
 	balanceText:=fmt.Sprint(balance)
-	os.WriteFile("balance.txt", []byte(balanceText), 0644)
+	os.WriteFile(accountBalanceFile, []byte(balanceText), 0644)
 }
+
+func getBalanceFromFile() float64  {
+	data, _ :=os.ReadFile(accountBalanceFile)
+	balanceText := string(data)
+	balance, _:=strconv.ParseFloat(balanceText, 64)
+	return balance
+}
+
 
 func main() {
 
-	var accountBalance = 1000.0
+	var accountBalance = getBalanceFromFile()
 
 	var choices = [4]string{"Check balance", "Deposit money", "Withdraw money", "Exit"}
 	fmt.Println("Welcome to the Bank of Job!")
