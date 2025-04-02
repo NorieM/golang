0e31ee40bd417478de5c2c006cf592105e645ed3
Norie <norie.macewan@tiscali.co.uk>
2025-04-02 17:28:57 +0100

    Added error handling to profit calculator

--- Go-CompleteGuide/profit-calculator/profit_calculator.go
+++ Go-CompleteGuide/profit-calculator/profit_calculator.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
 
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate:")
+	revenue, err := getUserInput("Revenue: ")
+
+	if err != nil {
+		panic("Invalid value entered for revenue!")
+	}
+
+	expenses, err := getUserInput("Expenses: ")
+	if err != nil {
+		panic("Invalid value entered for expenses!")
+	}
+
+	taxRate, err := getUserInput("Tax Rate:")
+
+	if err != nil {
+		panic("Invalid value entered for tax rate!")
+	}
 
 	ebt, profit, ratio := calculateReturn(revenue, expenses, taxRate)
 
@@ -17,11 +31,16 @@ func main() {
 	prettyPrint(ratio, "%.3f\n")
 }
 
-func getUserInput(prompt string) float64 {
+func getUserInput(prompt string) (float64, error) {
 	var inputValue float64
 	fmt.Print(prompt)
 	fmt.Scan(&inputValue)
-	return inputValue
+
+	if inputValue <=0 {
+		return 0, errors.New("invalid value entered")
+	}
+
+	return inputValue, nil
 }
 
 func calculateReturn(revenue, expenses, taxRate float64) (float64, float64, float64) {
