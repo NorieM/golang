33189f93e6e5069f7ef3841e8922f640e32a3ccb
Norie <norie.macewan@tiscali.co.uk>
2025-04-06 17:26:02 +0100

    Fixed some othrer stuff

--- Go-CompleteGuide/structs/user/user.go
+++ Go-CompleteGuide/structs/user/user.go
@@ -13,24 +13,24 @@ type User struct {
 	createdAt time.Time
 }
 
-func (u User) outputUserDetails() {
+func (u User) OutputUserDetails() {
 	fmt.Println(u.firstName, u.lastName, u.birthdate, u.createdAt)
 }
 
-func (u *User) clearUserName() {
+func (u *User) ClearUserName() {
 	u.firstName = ""
 	u.lastName = ""
 }
 
-func NewUser(firstName , lastName, birthdate string) (*User, error ){
+func NewUser(FirstName , LastName, Birthdate string) (*User, error ){
 
 	if FirstName == "" || LastName == "" || Birthdate == "" {
 		return nil, errors.New("first name, last name or birth date missing")
 	}
-	return User{
-		firstName: firstName,
-		lastName:  lastName,
-		birthdate: birthdate,
+	return &User{
+		firstName: FirstName,
+		lastName:  LastName,
+		birthdate: Birthdate,
 		createdAt: time.Now(),
 	}, nil
 }
