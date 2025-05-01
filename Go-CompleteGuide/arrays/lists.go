465b2c5439f44e8f786b65022796f190c09ca29e
Norie <norie.macewan@tiscali.co.uk>
2025-05-01 20:18:43 +0100

    Working with Arrays

---
+++ Go-CompleteGuide/arrays/lists.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var productNames [4]string = [4]string{"A book"}
+	prices := []float64{1.34, 334.3, 123.0, 20.0}
+	fmt.Println(prices)
+	fmt.Println(productNames)
+}
