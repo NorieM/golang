2df91bca77e416815a200e0a9a529c59b3cfb507
Norie <norie.macewan@tiscali.co.uk>
2025-05-13 23:16:41 +0100

    Did some refactoring and fixed some errors

--- Go-CompleteGuide/practice/practice.go
+++ Go-CompleteGuide/practice/practice.go
@@ -6,18 +6,9 @@ import "fmt"
 
 type Product struct {
 	title string
-	id string
+	id    string
 	price float64
 }
-
-func NewProduct(Title string, Id string, Price float64) (Product) {
-	return Product{
-		title: Title,
-		id: Id,
-		price: Price,
-	}
-}
-
 
 func main() {
 
@@ -33,43 +24,41 @@ func main() {
 	fmt.Println(hobbies[0])
 
 	//		- The second and third element combined as a new list
-	fmt.Println(hobbies[0:2])
+	fmt.Println(hobbies[1:3])
 
 	// 3) Create a slice based on the first element that contains
 	//		the first and second elements.
 	//		Create that slice in two different ways (i.e. create two slices in the end)
-	slice_1 := hobbies[0:2]
-	fmt.Println(slice_1)
-
-	slice_2 := hobbies[:2]
-	fmt.Println(slice_2)
+	mainHobbies := hobbies[0:2]
+	fmt.Println(mainHobbies)
 
 	// 4) Re-slice the slice from (3) and change it to contain the second
 	//		and last element of the original array.
-	slice_3 := slice_1[1:3]
+	slice_3 := mainHobbies[1:3]
 	fmt.Println(slice_3)
 
 	// 5) Create a "dynamic array" that contains your course goals (at least 2 goals)
-	course_goals:= []string{"Finish the course", "Finish some other courses"}
+	courseGoals := []string{"Finish the course", "Finish some other courses"}
 
-	fmt.Println(course_goals)
+	fmt.Println(courseGoals)
 
 	// 6) Set the second goal to a different one AND then add a third goal to that existing dynamic array
-	course_goals[1] = "A change of goal"
-	course_goals = append(course_goals, "New goal")
+	courseGoals[1] = "A change of goal"
+	courseGoals = append(courseGoals, "New goal")
 
-	fmt.Println(course_goals)
+	fmt.Println(courseGoals)
 
 	// 7) Bonus: Create a "Product" struct with title, id, price and create a
 	//		dynamic list of products (at least 2 products).
-	//		Then add a third product to the existing list of products.
-	products:= []Product{NewProduct("Product 1", "01", 10.99), NewProduct("Product 2", "02", 8.99)}
-
-	fmt.Println(products)
-
-	products = append(products, NewProduct("Product 3", "03", 9.99))
+	products := []Product{{"Product 1", "01", 10.99}, {"Product 2", "02", 8.99}}
 	
 	fmt.Println(products)
+	
+	//		Then add a third product to the existing list of products.
+	newProduct := Product{"Product 3", "03", 9.99}
 
-}	
+	products = append(products, newProduct)
 
+	fmt.Println(products)
+
+}
