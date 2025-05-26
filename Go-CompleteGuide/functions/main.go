5e596be4928307791fc6fdfed49720be27e03bc1
NorieM <norie.macewan@tiscali.co.uk>
2025-05-26 18:28:12 +0100

    Anonymous Functions

--- Go-CompleteGuide/functions/main.go
+++ Go-CompleteGuide/functions/main.go
@@ -2,48 +2,20 @@ package main
 
 import "fmt"
 
-type transformFn func(int) int
-
 func main() {
-	numbers := []int{1, 2, 3, 4}
-	moreNumbers := []int{5, 1, 5}
+	numbers := []int{1, 2, 3}
 
-	doubled := transformNumbers(&numbers, double)
-	fmt.Println(doubled)
-	tripled := transformNumbers(&numbers, triple)
-	fmt.Println(tripled)
+	transformed := transformNumbers(&numbers, func( n int ) int {return n *2})
 
-	transformFn1 := getTransformerFunction(&numbers)
-	transformFn2 := getTransformerFunction(&moreNumbers)
-
-	transformedNumbers := transformNumbers(&numbers, transformFn1) 
-
-	transformedMoreNumbers := transformNumbers(&moreNumbers, transformFn2)
-
-	fmt.Println(transformedNumbers)
-	fmt.Println(transformedMoreNumbers)
+	fmt.Println(transformed)
 }
 
-func transformNumbers(numbers *[]int, transform transformFn) []int {
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
+
 	for _, val := range *numbers {
 		dNumbers = append(dNumbers, transform(val))
 	}
+
 	return dNumbers
-}
-
-func getTransformerFunction(numbers *[]int) transformFn {
-	if (*numbers)[0] == 1 {
-		return double
-	} else {
-		return triple
-	}
-}
-
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number int) int {
-	return number * 3
 }
