4c952d45b05e3be9e3a2ef15ca726879a2972163
Norie <norie.macewan@tiscali.co.uk>
2025-11-05 19:50:39 +0000

    Added IOManager interfacce

--- Go-CompleteGuide/price-calculator/main.go
+++ Go-CompleteGuide/price-calculator/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	"example.com/price-calculator/cmdmanager"
-	"example.com/price-calculator/filemanager"
+	// "example.com/price-calculator/filemanager"
 	"example.com/price-calculator/prices"
 )
 
@@ -13,7 +13,7 @@ func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		cmdm := cmdmanager.New()
 
 		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
