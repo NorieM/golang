ee8afac047214ef2b078b958eafaf6f3b1374d10
Norie <norie.macewan@tiscali.co.uk>
2025-11-05 18:51:45 +0000

    Added command manager struct

--- Go-CompleteGuide/price-calculator/main.go
+++ Go-CompleteGuide/price-calculator/main.go
@@ -3,17 +3,20 @@ package main
 import (
 	"fmt"
 
+	"example.com/price-calculator/cmdmanager"
 	"example.com/price-calculator/filemanager"
 	"example.com/price-calculator/prices"
 )
 
 func main() {
-	
-	taxRates := []float64{0,0.07, 0.1, 0.15}
+
+	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		fm :=filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		cmdm := cmdmanager.New()
+
+		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
 		priceJob.Process()
 	}
 	fmt.Println("Prices sent to file!")
