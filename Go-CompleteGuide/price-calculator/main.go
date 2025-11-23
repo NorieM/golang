eb89071eac50a3a12912b08a0feb54f387a576a1
Norie <norie.macewan@tiscali.co.uk>
2025-11-23 13:44:20 +0000

    Updated error handling

--- Go-CompleteGuide/price-calculator/main.go
+++ Go-CompleteGuide/price-calculator/main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 
-	"example.com/price-calculator/cmdmanager"
-	// "example.com/price-calculator/filemanager"
+	// "example.com/price-calculator/cmdmanager"
+	"example.com/price-calculator/filemanager"
 	"example.com/price-calculator/prices"
 )
 
@@ -13,11 +13,17 @@ func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
 	for _, taxRate := range taxRates {
-		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		cmdm := cmdmanager.New()
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		// cmdm := cmdmanager.New()
 
-		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
-		priceJob.Process()
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		err:= priceJob.Process()
+		
+		if err != nil {
+			fmt.Println("Could not process job")
+			fmt.Println(err)
+		}
+
 	}
 	fmt.Println("Prices sent to file!")
 }
--- Go-CompleteGuide/price-calculator/prices/prices.go
+++ Go-CompleteGuide/price-calculator/prices/prices.go
@@ -14,29 +14,34 @@ type TaxIncludedPriceJob struct {
 	TaxIncludedPrices map[string]string   `json:"tax_includded_prices"`
 }
 
-func (job *TaxIncludedPriceJob) LoadData() {
+func (job *TaxIncludedPriceJob) LoadData() error {
 
 	lines, err := job.IOManager.ReadLines()
 
 	if err != nil {
 		fmt.Println(err)
-		return
+		return err
 	}
 
 	prices, err := conversion.StringsToFloats(lines)
 
 	if err != nil {
 		fmt.Println(err)
-		return
+		return err
 	}
 
 	job.InputPrices = prices
+	return nil
 
 }
 
-func (job *TaxIncludedPriceJob) Process() {
+func (job *TaxIncludedPriceJob) Process() error {
 
-	job.LoadData()
+	err:= job.LoadData()
+
+	if err != nil {
+		return err
+	}
 
 	result := make(map[string]string)
 
@@ -47,7 +52,7 @@ func (job *TaxIncludedPriceJob) Process() {
 	}
 
 	job.TaxIncludedPrices = result
-	job.IOManager.WriteResult(job)
+	return job.IOManager.WriteResult(job)
 
 }
 
