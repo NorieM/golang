08238752f7a17aa9de17557dc9fb244862ee894c
Norie <norie.macewan@tiscali.co.uk>
2025-12-14 15:43:15 +0000

    Updated Save method to insert into database

--- EventAPI/models/event.go
+++ EventAPI/models/event.go
@@ -1,21 +1,51 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"example.com/rest-api/db"
+)
 
 type Event struct {
-	ID          int
-	Name        string `binding:required`
-	Description string `binding:required`
-	Location    string `binding:required`
+	ID          int64
+	Name        string    `binding:required`
+	Description string    `binding:required`
+	Location    string    `binding:required`
 	DateTime    time.Time `binding:required`
-	UserID int	
+	UserID      int
 }
 
 var events = []Event{}
 
-func (e Event) Save() {
-	// TODO: add event to database
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `
+		INSERT INTO events(
+		name,
+		description,
+		location,
+		dateTime.
+		user_id
+		) VALUES (?,?,?,?,? )
+	`
+	stmt, err := db.DB.Prepare(query)
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+
+	e.ID = id
+
+	return err
 }
 
 func GetAllEvents() []Event {
